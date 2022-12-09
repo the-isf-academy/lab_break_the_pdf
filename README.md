86e0e0b0c5d6b5aced0e32f6fc28c80c2159041d
emma <eqbrown@isf.edu.hk>
2022-12-09 11:52:08 +0800

    init commit for mission

---
+++ README.md
@@ -0,0 +1,16 @@
+**Mission:** Break into the PDF 
+
+**Description:** You must break into the PDF and find the secret hidden in the text. The fastest group wins. 
+
+**Tasks:**
+0. Clone the repository into your `unit_cryptography` folder
+    - Look at the files 
+0. Open the file `decrypt_pdf.py`
+0. Break into the PDF
+0. Read the hidden message
+
+
+Hints:
+Encryption key hint: look carefully at the mission
+What Python libraries might exist to help? 
+
---
+++ decrypt_pdf.py
@@ -0,0 +1,25 @@
+from adjectives import adjectives
+from animals import animals
+from time import time
+# 💻 Import Library Below (hint: check the included files) 💻 
+
+
+
+# stores the current time to calculate total time to decrypt
+start_time = time()
+
+
+#  💻 YOUR CODE GOES HERE 💻 #
+
+
+
+
+
+
+
+
+# calculates total time to decrypt the pdf
+end_time = time()
+total_time_elapsed = round(end_time - start_time,2)
+print('-'*50)
+print('Total time to decrypt:', total_time_elapsed,'seconds')
