8878fb83f8a289093588cd3f3badce2d12b6a574
Emma <eqbrown@isf.edu.hk>
2024-06-03 15:30:36 +0800

    simplify code

--- decrypt_pdf.py
+++ decrypt_pdf.py
@@ -7,15 +7,9 @@ from PyPDF2 import PdfFileReader
 #  💻 You must edit the code below to solve the puzzle 💻 #
 
 
-start_time = time()                         # stores the current time to calculate total time to decrypt
+file = PdfFileReader('encrypted_pdf.pdf')   
 
-file = PdfFileReader('encrypted_pdf.pdf')   # opens the PDF 
-file.decrypt('password')                    # returns a 0 or 1, if the password was correct
-
-    
-
-# calculates total time to decrypt the pdf
-end_time = time()
-total_time_elapsed = round(end_time - start_time,2)
-print('-'*50)
-print(f'Total time to decrypt: {total_time_elapsed} seconds')
+if file.decrypt('password') == 1:                
+    print('password successful')
+else:
+    print('password unsuccesful')
